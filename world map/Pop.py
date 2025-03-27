bf35ecdb1c7b28078fbf34d3c6924a82e07e568d
Timotheenkwar <133169218+TimotheeNkwar@users.noreply.github.com>
2025-03-27 14:26:24 +0200

    Update Pop.py

--- world map/Pop.py
+++ world map/Pop.py
@@ -8,6 +8,8 @@ popu.rename(columns={"Country/Territory":"Country","CCA3":"Code"},inplace=True)
 popu.head()
 
 st.title("World Population By Years")
+
+
 popu['Country_Capital'] = popu['Country'] + ' - ' + popu['Capital']+ ' - ' + popu['Continent']
 popu['Combined_Z'] = popu['1970 Population'] + popu['World Population Percentage']
 #natural earth,conic equal area,azimuthal equal area,orthographic,mercator
